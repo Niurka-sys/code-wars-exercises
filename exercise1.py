cd2c19b9366c813cc77dd32ccaf838cfd670de7e
NIURK <{romerorojasn@gmail.com}>
2025-01-18 18:42:26 -0400

    Mensaje del commit

---
+++ exercise1.py
@@ -0,0 +1,17 @@
+
+"""Create a function that takes an integer
+as an argument and returns "Even" for even numbers or "Odd" for odd numbers."""
+
+def even_or_odd(number):
+    if number % 2 == 0:
+        return "Even"
+    else:
+        return "Odd"  # return "Odd" if the number is not even
+    
+# Test cases to validate the function     
+print(even_or_odd(2))  # Even           
+print(even_or_odd(3))  # Odd
+
+# Output    
+# Even
+# Odd
